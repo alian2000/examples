a8612fb3138892d67f95b5cd839303b9dccc0b33
Usama Alian <engalian@gmail.com>
2023-08-26 07:29:47 -0500

    Create wizard-list-1.py

---
+++ wizard-list-1.py
@@ -0,0 +1,36 @@
+fred = 50
+john = fred
+
+
+print(john)
+
+message = 'I scored %s points'
+print(message % john)
+
+
+#wizard_list = 'spider legs, toe of frog, eye of newt, bat wing,slug butter, snake dandruff'
+wizard_list_2 = ["spider legs","toe of frog", "eye of newt","bat wing","slug butter"," snake dandruff"]
+
+#print(len(wizard_list))
+print(len(wizard_list_2))
+print(wizard_list_2[2:5])
+
+#print(wizard_list[13])
+print(wizard_list_2[3])
+#print(wizard_list)
+#print(wizard_list_2)
+
+
+for i in (wizard_list_2):
+    print(i)
+    
+x="usama@gmail.com"
+print(x[0:5])
+
+wizard_list_2.append('Avatar')
+print(wizard_list_2)
+
+del wizard_list_2[0:7]
+print(wizard_list_2)
+
+    
