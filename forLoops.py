4dfeded5b79377119685b4b8e26087084befd319
Usama Alian <engalian@gmail.com>
2023-08-26 07:31:00 -0500

    Create forLoops.py

---
+++ forLoops.py
@@ -0,0 +1,17 @@
+import os
+
+for x in range(0, 5):
+    print('hello')
+    print('hello %s' % x)
+    
+print(list(range(0, 5)))
+
+
+wizard_list = ["spider legs","toe of frog", "eye of newt","bat wing","slug butter"," snake dandruff"]
+
+for i in (wizard_list):
+    print(i)
+
+for x in "banana":
+  print(x)
+  
