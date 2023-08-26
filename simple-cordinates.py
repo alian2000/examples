3ecc4d793598a057812844d1efde1acd58857efc
Usama Alian <engalian@gmail.com>
2023-08-25 22:00:56 -0500

    Add files via upload

---
+++ simple-cordinates.py
@@ -0,0 +1,24 @@
+import json
+location=[45.523, -122.675]   # latitude,longtitude cordinates as a json 
+print (location)
+print (location[0])
+
+x={
+  
+        "coordinates": [
+          [
+            35.225873472283126,
+            31.780518532844624
+          ],
+          [
+            35.23096421486548,
+            31.903769891552272
+          ]]}
+
+print (x['coordinates'])
+print(x['coordinates'][0])
+print(x['coordinates'][1])
+
+print (len(['coordinates']))
+
+
