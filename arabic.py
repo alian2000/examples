8561cee77c00787bf72ed2fe7f3e9cbf3a2949f4
Usama Alian <engalian@gmail.com>
2023-08-26 07:47:26 -0500

    Add files via upload

---
+++ arabic.py
@@ -0,0 +1,27 @@
+
+
+wizard_list_2 = "موقع كتابة عربي في الفوتوشوب هو الأداة التي يحتاجها جميع مستعملي الحاسوب"
+
+#print(len(wizard_list_2))
+print(wizard_list_2[11:15])                                       #to reverse [::-1])
+print("-------------------------------------------")
+
+
+
+
+print(wizard_list_2[0])
+print(wizard_list_2[1])
+print(wizard_list_2[2])
+print(wizard_list_2[3])
+print(wizard_list_2[4])
+print(wizard_list_2[5])
+print("-------------------------------------")
+
+
+for i in (wizard_list_2):
+    print(i)
+    
+
+
+
+    
---
+++ basic-list-array.py
@@ -0,0 +1,43 @@
+fred = 200
+john = fred
+print(john)
+message = 'I scored %s points'
+print(message % john)
+
+
+wizard_list = 'spider legs, toe of frog, eye of newt, bat wing,slug butter, snake dandruff'
+
+
+
+
+
+
+
+
+print(len(wizard_list))
+print(len(wizard_list))
+print(wizard_list[2:5])
+
+#print(wizard_list[13])
+print(wizard_list[0])
+print(wizard_list[1])
+print(wizard_list[2])
+print(wizard_list[3])
+print(wizard_list[4])
+print(wizard_list[5])
+print("-------------------------------------")
+#print(wizard_list)
+#print(wizard_list_2)
+
+
+for i in (wizard_list):
+    print(i)
+    
+x="usama@gmail.com"
+print(x[5])
+
+
+
+
+
+    
