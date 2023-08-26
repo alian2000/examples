eb6c0f0e7a2963725c4a924d350aed80f3bc48be
Usama Alian <engalian@gmail.com>
2023-08-26 06:46:05 -0500

    Add files via upload

---
+++ list2.py
@@ -0,0 +1,17 @@
+
+student = "Usama Alian"
+print (student)
+print(student[0:5])
+print(student[0:3])
+#print(student[6:11])
+print(len(student))
+
+
+print(student[0:len(student)])
+
+
+
+
+
+
+    
