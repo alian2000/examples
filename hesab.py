a40ec549acf04807889f16b8f0ee9273fcd18769
Usama Alian <engalian@gmail.com>
2023-10-27 06:01:50 -0500

    Add files via upload

---
+++ hesab.py
@@ -0,0 +1,25 @@
+import math
+def my_function(first,lastname):
+  print(first,lastname,"Hello world")
+  
+#my_function("nour","Domani")
+
+# Python program to find Area of a circle
+# using inbuild library
+
+
+
+def rectangle_area(base, height):
+    area = base*height
+    print("The rectangular area is " + str(area))
+
+def circle(r):
+  area = math.pi* pow(r,2)
+  return int(area)
+
+def squarArea(side):
+    areasq= pow(side,2)
+    print("areasquare= ",(areasq))
+
+
+
---
+++ subfunc.py
@@ -0,0 +1,14 @@
+import hesab
+
+
+hesab.rectangle_area(10,12)
+hesab.circle (4)
+hesab.squarArea(3)
+
+housearea=(hesab.circle(4))
+print (housearea)
+print ("total_house_area =",(housearea+100))
+
+
+
+
