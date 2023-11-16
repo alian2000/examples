5154c6200ccdaea8d8b6d21ab2d5ffb2f8eff577
Usama Alian <engalian@gmail.com>
2023-11-16 07:49:26 -0600

    Create builtin-functions.py

---
+++ builtin-functions.py
@@ -0,0 +1,15 @@
+list_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+element = 10
+print (list_numbers.index(element)) # find index position of number 10
+print (list_numbers.index(10))  # give me the position of the value
+print (list_numbers[9])   #valuse in position 9
+print (len(list_numbers))   # watch how to use the length function
+
+input = ['Python', 'Java', 'Ruby', 'JavaScript']
+size = len(input)
+print(size)
+print (input.index("Java"))
+print (input[3].upper())
+for language in input:
+    print (language)
+    print(language.lower())
